ab0959dbb590b68b72d613f8a5acf7c1a5a1226f
SamChen12390 <s3697216@student.rmit.edu.au>
2020-08-30 12:17:49 +1000

    Fix
    Delete Fish folder..

--- Fish/Assets/Scripts/Fish/FilteredFlockBehaviour.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class FilteredFlockBehaviour : FishBehaviour
-{
-
-    public ContextFilter filter;
-}
